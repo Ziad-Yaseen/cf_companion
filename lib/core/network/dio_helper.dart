35b963ae31d9693235726c3461cda8b72d467deb
Ziad-Yaseen <ziadyaseen.dev@gmail.com>
2026-09-15 16:48:30 +0300

    dio helper Rate Limiting

--- lib/core/network/dio_helper.dart
+++ lib/core/network/dio_helper.dart
@@ -3,6 +3,8 @@ import 'package:dio/dio.dart';
 
 class DioHelper {
   static Dio? dio;
+  static DateTime _lastRequestTime = DateTime.fromMillisecondsSinceEpoch(0);
+  static const Duration _rateLimitDuration = Duration(seconds: 2);
 
   static void initDio() {
     dio ??= Dio(
@@ -19,6 +21,16 @@ class DioHelper {
     required String endPoint,
     required Map<String, dynamic> queryParameters,
   }) async {
+    final now = DateTime.now();
+    final nextAvailableTime = _lastRequestTime.add(_rateLimitDuration);
+
+    if (now.isBefore(nextAvailableTime)) {
+      final waitDuration = nextAvailableTime.difference(now);
+      _lastRequestTime = nextAvailableTime;
+      await Future.delayed(waitDuration);
+    } else {
+      _lastRequestTime = now;
+    }
     try {
       Response response = await dio!.get(
         endPoint,
