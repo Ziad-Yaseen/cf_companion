46b527e22eeb3377b6924cc1bf8a0374d08a999d
Ziad-Yaseen <ziadyaseen.dev@gmail.com>
2026-09-15 14:44:25 +0300

    app sizes

---
+++ lib/core/constants/app_sizes.dart
@@ -0,0 +1,97 @@
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class AppSizes {
+  // Heights
+  static double height1 = 1.h;
+  static double height5 = 5.h;
+  static double height10 = 10.h;
+  static double height15 = 15.h;
+  static double height20 = 20.h;
+  static double height25 = 25.h;
+  static double height30 = 30.h;
+  static double height35 = 35.h;
+  static double height40 = 40.h;
+  static double height45 = 45.h;
+  static double height50 = 50.h;
+  static double height55 = 55.h;
+  static double height60 = 60.h;
+  static double height65 = 65.h;
+  static double height70 = 70.h;
+  static double height75 = 75.h;
+  static double height80 = 80.h;
+  static double height85 = 85.h;
+  static double height90 = 90.h;
+  static double height95 = 95.h;
+  static double height96 = 96.h;
+  static double height100 = 100.h;
+
+  // Widths
+  static double width1 = 1.w;
+  static double width5 = 5.w;
+  static double width10 = 10.w;
+  static double width15 = 15.w;
+  static double width20 = 20.w;
+  static double width25 = 25.w;
+  static double width30 = 30.w;
+  static double width35 = 35.w;
+  static double width40 = 40.w;
+  static double width45 = 45.w;
+  static double width50 = 50.w;
+  static double width55 = 55.w;
+  static double width60 = 60.w;
+  static double width65 = 65.w;
+  static double width70 = 70.w;
+  static double width75 = 75.w;
+  static double width80 = 80.w;
+  static double width85 = 85.w;
+  static double width90 = 90.w;
+  static double width95 = 95.w;
+  static double width96 = 96.w;
+  static double width100 = 100.w;
+
+  // Font Sizes
+  static double fontSize10 = 10.sp;
+  static double fontSize12 = 12.sp;
+  static double fontSize14 = 14.sp;
+  static double fontSize16 = 16.sp;
+  static double fontSize18 = 18.sp;
+  static double fontSize20 = 20.sp;
+  static double fontSize22 = 22.sp;
+  static double fontSize24 = 24.sp;
+  static double fontSize26 = 26.sp;
+  static double fontSize28 = 28.sp;
+  static double fontSize30 = 30.sp;
+  static double fontSize32 = 32.sp;
+  static double fontSize34 = 34.sp;
+  static double fontSize36 = 36.sp;
+  static double fontSize38 = 38.sp;
+  static double fontSize40 = 40.sp;
+
+  // Border Radius
+  static double borderRadius4 = 4.r;
+  static double borderRadius8 = 8.r;
+  static double borderRadius12 = 12.r;
+  static double borderRadius14 = 14.r;
+  static double borderRadius16 = 16.r;
+  static double borderRadius20 = 20.r;
+  static double borderRadius24 = 24.r;
+  static double borderRadius28 = 28.r;
+
+  // Icon Sizes
+  static double iconSize16 = 16.sp;
+  static double iconSize20 = 20.sp;
+  static double iconSize24 = 24.sp;
+  static double iconSize28 = 28.sp;
+  static double iconSize32 = 32.sp;
+
+  // Padding
+  static const double padding4 = 4;
+  static const double padding8 = 8;
+  static const double padding12 = 12;
+  static const double padding16 = 16;
+  static const double padding20 = 20;
+  static const double padding24 = 24;
+  static const double padding28 = 28;
+  static const double padding32 = 32;
+
+}
---
+++ lib/features/splash/widgets/app_icon.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppIconContainer extends StatelessWidget {
+  const AppIconContainer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
