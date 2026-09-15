4a68ac9e7f357d8a647914efa3ab30c66ec0473d
Ziad-Yaseen <ziadyaseen.dev@gmail.com>
2026-09-15 13:45:43 +0300

    app icons

---
+++ lib/core/constants/app_icons.dart
@@ -0,0 +1,116 @@
+import 'package:flutter/material.dart';
+import 'package:lucide_icons/lucide_icons.dart';
+
+class AppIcons {
+  AppIcons._();
+
+  // ---------------------------------------------------------------------
+  // Bottom Navigation
+  // ---------------------------------------------------------------------
+  static const IconData navProfile = LucideIcons.user;
+  static const IconData navProfileActive = LucideIcons.user;
+
+  static const IconData navProblems = LucideIcons.listChecks;
+  static const IconData navSubmissions = LucideIcons.fileText;
+  static const IconData navContests = LucideIcons.trophy;
+  static const IconData navMore = LucideIcons.moreHorizontal;
+
+  // ---------------------------------------------------------------------
+  // Profile Screen
+  // ---------------------------------------------------------------------
+  static const IconData settings = LucideIcons.settings;
+  static const IconData solvedCheck = LucideIcons.checkCircle2;
+  static const IconData contribution = LucideIcons.trendingUp;
+  static const IconData registeredSince = LucideIcons.calendar;
+  static const IconData lastSeen = LucideIcons.clock;
+  static const IconData streakFlame = LucideIcons.flame;
+  static const IconData chartDetails = LucideIcons.chevronLeft; // RTL
+
+  // ---------------------------------------------------------------------
+  // Problems Screen
+  // ---------------------------------------------------------------------
+  static const IconData search = LucideIcons.search;
+  static const IconData clearText = LucideIcons.x;
+  static const IconData filter = LucideIcons.slidersHorizontal;
+  static const IconData bookmarkAdd = LucideIcons.bookmarkPlus;
+  static const IconData solvedIndicator = LucideIcons.checkCircle2;
+  static const IconData unsolvedIndicator = LucideIcons.circle;
+  static const IconData emptySearch = LucideIcons.searchX;
+
+  // ---------------------------------------------------------------------
+  // Submissions Screen
+  // ---------------------------------------------------------------------
+  static const IconData verdictAccepted = LucideIcons.check;
+  static const IconData verdictWrong = LucideIcons.x;
+  static const IconData verdictTimeLimit = LucideIcons.clock;
+  static const IconData verdictOther = LucideIcons.alertTriangle;
+  static const IconData performanceWarning = LucideIcons.alertTriangle;
+  static const IconData language = LucideIcons.code2;
+  static const IconData memory = LucideIcons.memoryStick;
+  static const IconData emptySubmissions = LucideIcons.fileX;
+  static const IconData externalLink = LucideIcons.externalLink;
+  static const IconData copyId = LucideIcons.copy;
+
+  // ---------------------------------------------------------------------
+  // Contests Screen
+  // ---------------------------------------------------------------------
+  static const IconData reminderOff = LucideIcons.bell;
+  static const IconData reminderOn = LucideIcons.bellRing;
+  static const IconData duration = LucideIcons.timer;
+  static const IconData liveIndicator = LucideIcons.radio;
+  static const IconData standings = LucideIcons.barChart3;
+  static const IconData rankUp = LucideIcons.arrowUp;
+  static const IconData rankDown = LucideIcons.arrowDown;
+  static const IconData emptyContests = LucideIcons.activity;
+  static const IconData chevronForward = LucideIcons.chevronLeft; // RTL
+
+  // ---------------------------------------------------------------------
+  // More / Hub Screen
+  // ---------------------------------------------------------------------
+  static const IconData trainingPlans = LucideIcons.bookmark;
+  static const IconData upsolve = LucideIcons.rotateCcw;
+  static const IconData tagAnalyzer = LucideIcons.radar;
+  static const IconData compareHandles = LucideIcons.users;
+  static const IconData teamMode = LucideIcons.userPlus;
+  static const IconData vip = LucideIcons.crown;
+  static const IconData feedback = LucideIcons.messageSquare;
+
+  // ---------------------------------------------------------------------
+  // Settings Screen
+  // ---------------------------------------------------------------------
+  static const IconData changeHandle = LucideIcons.userCog;
+  static const IconData darkMode = LucideIcons.moon;
+  static const IconData notificationDefault = LucideIcons.bellRing;
+  static const IconData dailyGoal = LucideIcons.target;
+  static const IconData version = LucideIcons.info;
+  static const IconData decrement = LucideIcons.minus;
+  static const IconData increment = LucideIcons.plus;
+
+  // ---------------------------------------------------------------------
+  // Dialogs / Generic
+  // ---------------------------------------------------------------------
+  static const IconData delete = LucideIcons.trash2;
+  static const IconData warningDialog = LucideIcons.alertTriangle;
+  static const IconData confirmChange = LucideIcons.userCircle2;
+  static const IconData toastSuccess = LucideIcons.checkCircle2;
+  static const IconData toastError = LucideIcons.alertCircle;
+  static const IconData toastInfo = LucideIcons.info;
+  static const IconData undo = LucideIcons.undo2;
+  static const IconData rename = LucideIcons.pencil;
+  static const IconData kebabMenu = LucideIcons.moreVertical;
+
+  // ---------------------------------------------------------------------
+  // Error / Empty States
+  // ---------------------------------------------------------------------
+  static const IconData noInternet = LucideIcons.wifiOff;
+  static const IconData genericError = LucideIcons.serverCrash;
+  static const IconData refresh = LucideIcons.refreshCw;
+  static const IconData rateLimit = LucideIcons.hourglass;
+
+  // ---------------------------------------------------------------------
+  // Onboarding
+  // ---------------------------------------------------------------------
+  static const IconData handleInput = LucideIcons.atSign;
+  static const IconData onboardingSuccess = LucideIcons.checkCheck;
+  static const IconData forwardArrow = LucideIcons.arrowLeft; // RTL
+}
