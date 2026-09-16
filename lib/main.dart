f7cef0ddc9cd435113fa14e49d214aed3fed1d61
Ziad-Yaseen <ziadyaseen.dev@gmail.com>
2026-09-16 11:18:00 +0300

    service locator get_it

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:cf_companion/app.dart';
+import 'package:cf_companion/core/di/service_locator.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
+  setupLocator();
   runApp(const Delta());
 }
