67fb40bb061b25a17bf06ee0c9ac8912bf37f8f6
Ziad-Yaseen <ziadyaseen.dev@gmail.com>
2026-09-16 10:47:04 +0300

    test

--- lib/features/splash/screens/splash_screen.dart
+++ lib/features/splash/screens/splash_screen.dart
@@ -3,7 +3,9 @@ import 'dart:developer';
 import 'package:cf_companion/core/constants/app_sizes.dart';
 import 'package:cf_companion/core/network/api_exception.dart';
 import 'package:cf_companion/core/repositories/user_info_repository.dart';
+import 'package:cf_companion/core/repositories/user_rating_repository.dart';
 import 'package:cf_companion/core/services/user_info_service.dart';
+import 'package:cf_companion/core/services/user_rating_service.dart';
 import 'package:cf_companion/core/styles/text_styles.dart';
 import 'package:cf_companion/features/splash/widgets/app_icon.dart';
 import 'package:cf_companion/features/splash/widgets/splash_progress_indicator.dart';
@@ -13,11 +15,30 @@ import 'package:gap/gap.dart';
 class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
 
-  Future<void> getUserInfo() async {
+  Future<void> _testUserInfo() async {
     try {
       final user = UserInfoRepository(UserInfoService());
       final userInfo = await user.fetchUserInfo('Dark_Zid');
       log(userInfo.toString());
+    } on ApiException catch (e) {
+      log('API Error: ${e.message}');
+    } catch (e) {
+      log('Unexpected error: $e');
+    }
+  }
+
+  Future<void> _testUserRating() async {
+    try {
+      final repo = UserRatingRepository(UserRatingService());
+      final ratingHistory = await repo.fetchRatingHistory('Dark_Zid');
+
+      log('عدد الكونتستات: ${ratingHistory.length}');
+      for (final change in ratingHistory) {
+        log(
+          '${change.contestName} | ${change.oldRating} -> ${change.newRating} '
+          '(${change.isPositive ? '+' : ''}${change.delta})',
+        );
+      }
     } on ApiException catch (e) {
       log('API Error: ${e.message}');
     } catch (e) {
@@ -59,7 +80,8 @@ class SplashScreen extends StatelessWidget {
 
       floatingActionButton: FloatingActionButton(
         onPressed: () async {
-          await getUserInfo();
+          await _testUserInfo();
+          await _testUserRating();
         },
       ),
     );
