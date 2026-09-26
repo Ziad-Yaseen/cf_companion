5f38dd6eb0948adf23efc255e4a32653580f59ec
Ziad-Yaseen <ziadyaseen.dev@gmail.com>
2026-09-26 15:21:14 +0300

    create heatmap widget

---
+++ lib/features/profile/widgets/app_heatmap_widget.dart
@@ -0,0 +1,10 @@
+  import 'package:flutter/material.dart';
+
+  class AppHeatmapWidget extends StatelessWidget {
+    const AppHeatmapWidget({super.key});
+
+    @override
+    Widget build(BuildContext context) {
+      return const Placeholder();
+    }
+  }
